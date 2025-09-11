549b01ec97ece50bc93f37ad294c0cf377b758dc
HarisYounsi <haris.younsi12@gmail.com>
2025-09-11 13:17:38 +0200

    modif vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',                    // essentiel pour le bon chargement
+  base: './',                    // essentiel pour le bon chargement
   plugins: [react()],
   build: {
     outDir: 'dist',
